79dcde45207e6f60866e9d88949d942fed01a431
Kyrstle Breslin <kyrstle@live.com.au>
2018-08-11 17:44:51 +0100

    updated shelf function - still issues with adding book from search

--- src/App.js
+++ src/App.js
@@ -10,30 +10,31 @@ import Search from './Search.js'
 class BooksApp extends Component {
   constructor(props) {
     super(props);
+    this.handleChangeShelf = this.changeShelf.bind(this);
+  }
 
-  this.handleChangeShelf = this.changeShelf.bind(this);
-}
+  state = {books: []}
 
-state = {books: []}
+  componentDidMount = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    });
+  }
 
-componentDidMount = () => {
-  BooksAPI.getAll().then((books) => {
-    this.setState({books})
-  });
-}
-
-
-  changeShelf = (book, shelf) => {
-    console.log('function is passing')
-  //   BooksAPI.update({id: book.id}, shelf).then(() => {
-  //     book.shelf = shelf
-  //     this.setState(state => ({
-  //       books: state.books.filter(b => b.id !==book.id.concat(book))
-  //     }))
-  //   })
-
-
-  };
+  changeShelf = (newBook, shelf) => {
+    let newShelf = newBook.target.value;
+    let newBookID = newBook.target.id;
+    BooksAPI.update(newBook, shelf).then(change => {
+      BooksAPI.getAll().then((books) => {
+        let thisBook = books.filter((book) => (newBookID === book.id))
+        thisBook[0].shelf = newShelf
+        let newShelvedBooks = this.state.books.filter((book) => (newBookID !== book.id))
+        newShelvedBooks.push(thisBook[0]);
+        this.setState({books: newShelvedBooks})
+        thisBook = [];
+      })
+    })
+  }
 
 
   render() {
@@ -41,7 +42,9 @@ componentDidMount = () => {
       <div className="app">
         <Route exact path ='/' render={() => (<BookShelf books={this.state.books} updateShelf={this.changeShelf}/>
         )}/>
-      <Route path='/search' render={() => (<Search updateShelf={this.changeShelf}/>)}/>
+      <Route path='/search' render={() => (<Search updateShelf={this.changeShelf}
+      handlingClick={this.handleClick}/>
+    )}/>
       </div>
     )
   }
--- src/Book.js
+++ src/Book.js
@@ -13,7 +13,7 @@ class Book extends React.Component {
             <div className="book-cover" style={{ width: 128, height: 188, backgroundImage:`url(${book.imageLinks.thumbnail})`}}>
             </div>
             <div className="book-shelf-changer">
-              <select onChange={this.props.toUpdateShelf}>
+              <select id={book.id} value={book.shelf} onChange={this.props.toUpdateShelf}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
