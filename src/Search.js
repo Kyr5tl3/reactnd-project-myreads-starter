587212cf747cea250aa0c16f27d56a391c4d3605
Kyrstle Breslin <kyrstle@live.com.au>
2018-08-11 10:11:50 +0100

    add link to go back to homepage

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import './App.css'
 import Book from './Book.js'
 import PropTypes from 'prop-types'
@@ -6,11 +7,21 @@ import PropTypes from 'prop-types'
 
 class Search extends Component {
 
+  constructor(){
+    super();
+    this.state = {
+      searchedBooks: []
+    };
+  }
+
+  componentDidMount() {
+
+  }
   render(){
     return(
         <div className="search-books">
           <div className="search-books-bar">
-            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+            <Link className="close-search" to="/">Close</Link>
             <div className="search-books-input-wrapper">
               {/*
                 NOTES: The search from BooksAPI is limited to a particular set of search terms.
